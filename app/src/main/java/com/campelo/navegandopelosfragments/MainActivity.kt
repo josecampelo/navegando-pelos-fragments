53a43b7690aa2741200e6d2fe81dc65586754999
José Campelo <josecampelojr@gmail.com>
2023-07-01 11:36:55 -0300

    Finalizando o projeto, criando a lógica da criação dos fragments e sua navegação.

--- app/src/main/java/com/campelo/navegandopelosfragments/MainActivity.kt
+++ app/src/main/java/com/campelo/navegandopelosfragments/MainActivity.kt
@@ -2,10 +2,40 @@ package com.campelo.navegandopelosfragments
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnConversas: Button
+    private lateinit var btnStatus: Button
+    private lateinit var btnChamadas: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnConversas = findViewById(R.id.btnConversas)
+        btnStatus = findViewById(R.id.btnStatus)
+        btnChamadas = findViewById(R.id.btnChamadas)
+
+        btnConversas.setOnClickListener {
+            onFragment(ConversasFragment())
+        }
+
+        btnStatus.setOnClickListener {
+            onFragment(StatusFragment())
+        }
+
+        btnChamadas.setOnClickListener {
+            onFragment(ChamadasFragment())
+        }
+    }
+
+    private fun onFragment(fragment: Fragment){
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragmentConteudo, fragment)
+            .commit()
     }
 }
